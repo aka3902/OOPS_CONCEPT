c27498142bacfff599208202370a900457f858c7
arpit <arpitk2453@gmail.com>
2024-10-09 19:32:07 +0530

    check

---
+++ class_intro.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+class Teacher{
+
+    public:
+    string name;
+    string dept;
+    string subjects;
+    float salary;
+
+   
+};
+
+int main()
+{
+ 
+  Teacher t1;
+  t1.name="Arpit";
+  t1.dept="CSE";
+  t1.subjects="C++";
+  t1.salary=85000;
+  
+
+   cout<<t1.name<<" ki salary-> "<<t1.salary<<" and dept:"<<t1.dept <<" and subjects "<<t1.subjects;
+   
+   
+
+     return 0;
+
+
+
+
+
+   
+}
